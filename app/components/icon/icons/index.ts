1fcc3bde8ad00fa606bb0f8df89f6807bb640563
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-12-16 20:14:16 -0500

    add info icon

--- app/components/icon/icons/index.ts
+++ app/components/icon/icons/index.ts
@@ -9,7 +9,8 @@ export const icons = {
   shuffle: require("./shuffle.png"),
   search: require("./search.png"),
   refreshCw: require("./refresh-cw.png"),
-  mapPin: require("./map-pin.png")
+  mapPin: require("./map-pin.png"),
+  info: require("./info.png")
 }
 
 export type IconTypes = keyof typeof icons
