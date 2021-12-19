241f1a0df142a5735322dbfe27ffdb67278b8b5d
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-12-19 15:16:06 -0500

    rm demo screens, assets, stores, models...

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -1,13 +1,11 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
 import { SpeciesStoreModel } from "../species-store/species-store"
-import { CharacterStoreModel } from "../character-store/character-store"
 
 /**
  * A RootStore model.
  */
 // prettier-ignore
 export const RootStoreModel = types.model("RootStore").props({
-  characterStore: types.optional(CharacterStoreModel, {} as any),
   speciesStore: types.optional(SpeciesStoreModel, {} as any)
 })
 
--- app/services/api/api.types.ts
+++ app/services/api/api.types.ts
@@ -1,5 +1,4 @@
 import { GeneralApiProblem } from "./api-problem"
-import { Character } from "../../models/character/character"
 import { Species } from "../../models/species/species"
 
 export interface User {
@@ -9,8 +8,5 @@ export interface User {
 
 export type GetUsersResult = { kind: "ok"; users: User[] } | GeneralApiProblem
 export type GetUserResult = { kind: "ok"; user: User } | GeneralApiProblem
-
-export type GetCharactersResult = { kind: "ok"; characters: Character[] } | GeneralApiProblem
-export type GetCharacterResult = { kind: "ok"; character: Character } | GeneralApiProblem
 
 export type GetSpeciesResult = { kind: "ok"; species: Species[] } | GeneralApiProblem
