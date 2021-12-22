5126d21915b229e9469e58780a7f090981282cbb
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-12-21 19:29:13 -0500

    rm unused import

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -7,7 +7,6 @@ import { spacing, color } from "../../theme"
 import { observer } from "mobx-react-lite"
 import { useStores } from "../../models"
 import { useNavigation } from '@react-navigation/native';
-import { SearchScreen } from "../../screens"
 import { capitalize } from "lodash"
 
 // static styles
