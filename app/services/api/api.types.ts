4b91387a9e77c5e3bb09887e5ab1b6cada998361
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-12-21 19:39:12 -0500

    rm demo user api logic

--- app/services/api/api.types.ts
+++ app/services/api/api.types.ts
@@ -2,14 +2,9 @@ import { GeneralApiProblem } from "./api-problem"
 import { Species } from "../../models/species/species"
 import { Pokemon } from "../../models/pokemon/pokemon"
 
-export interface User {
-  id: number
-  name: string
-}
-
-export type GetUsersResult = { kind: "ok"; users: User[] } | GeneralApiProblem
-export type GetUserResult = { kind: "ok"; user: User } | GeneralApiProblem
-
+// Species
 export type GetAllSpeciesResult = { kind: "ok"; species: Species[] } | GeneralApiProblem
 export type GetSpeciesResult = { kind: "ok"; species: Species } | GeneralApiProblem
+
+// Pokemon
 export type GetPokemonResult = { kind: "ok"; pokemon: Pokemon } | GeneralApiProblem
