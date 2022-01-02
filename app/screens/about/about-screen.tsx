4654626f1a2a14d9bf6427e7701ea5e5d335d5e9
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2022-01-02 14:30:15 -0500

    add logos to about screen, stub links

--- app/screens/about/about-screen.tsx
+++ app/screens/about/about-screen.tsx
@@ -1,15 +1,28 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { Screen, Text } from "../../components"
+import { ViewStyle, ImageStyle, Pressable, View, TouchableOpacityProps } from "react-native"
+import { Screen, Text, Icon, AutoImage as Image } from "../../components"
+import { TxKeyPath } from "../../i18n"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
 
+const appLogo = require("./app-logo.png")
+const pokeapiLogo = require("./pokeapi-logo.png")
+
 const ROOT: ViewStyle = {
   backgroundColor: color.background,
   flex: 1,
-  alignItems: "center"
+  alignItems: "center",
+}
+const IMAGE: ImageStyle = {
+  width: 150,
+  resizeMode: "contain"
+}
+
+interface LinkProps extends TouchableOpacityProps {
+  titleTx: TxKeyPath,
+  url: string
 }
 
 export const AboutScreen = observer(function AboutScreen() {
@@ -18,9 +31,36 @@ export const AboutScreen = observer(function AboutScreen() {
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  function Link(props: LinkProps) {
+    // TODO: get subtitle from url
+    const subtitle = "TODO"
+    return (
+      <Pressable>
+        <View>
+          <Text tx={props.titleTx} />
+          <Text text={subtitle} />
+        </View>
+        <View>
+          <Icon icon="chevronRight" />
+        </View>
+      </Pressable>
+    )
+  }
+
   return (
     <Screen style={ROOT} preset="scroll">
       <Text preset="header" tx="aboutScreen.title" />
+      <Image source={appLogo} style={IMAGE} />
+      {/* Version */}
+      <View>
+        <Link
+          titleTx="aboutScreen.developer"
+          url="https://simpledex.cavender.io/" />
+        <Link
+          titleTx="aboutScreen.developer"
+          url="https://simpledex.cavender.io/" />
+      </View>
+      <Image source={pokeapiLogo} style={IMAGE} />
     </Screen>
   )
 })
