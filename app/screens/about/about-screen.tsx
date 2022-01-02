bf557af67d7de480e8556a0a5e647b39148ccc47
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2022-01-02 16:09:45 -0500

    better format iamges on about screen, add i18n copy for links

--- app/screens/about/about-screen.tsx
+++ app/screens/about/about-screen.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle, ImageStyle, Pressable, View, TouchableOpacityProps } from "react-native"
+import { ViewStyle, ImageStyle, TextStyle, Pressable, View, TouchableOpacityProps } from "react-native"
 import { Screen, Text, Icon, AutoImage as Image } from "../../components"
 import { TxKeyPath } from "../../i18n"
 // import { useNavigation } from "@react-navigation/native"
@@ -14,10 +14,20 @@ const ROOT: ViewStyle = {
   backgroundColor: color.background,
   flex: 1,
   alignItems: "center",
+  justifyContent: "space-between"
 }
-const IMAGE: ImageStyle = {
-  width: 150,
+const LOGO: ImageStyle = {
   resizeMode: "contain"
+}
+const APP_LOGO: ImageStyle = {
+  height: 150,
+}
+const API_TEXT: TextStyle = {
+  textAlign: "center"
+}
+const API_LOGO: ImageStyle = {
+  height: 50,
+  marginBottom: 50
 }
 
 interface LinkProps extends TouchableOpacityProps {
@@ -50,17 +60,20 @@ export const AboutScreen = observer(function AboutScreen() {
   return (
     <Screen style={ROOT} preset="scroll">
       <Text preset="header" tx="aboutScreen.title" />
-      <Image source={appLogo} style={IMAGE} />
+      <Image source={appLogo} style={[LOGO, APP_LOGO]} />
       {/* Version */}
       <View>
         <Link
           titleTx="aboutScreen.developer"
-          url="https://simpledex.cavender.io/" />
+          url="https://dev.cavender.io" />
         <Link
-          titleTx="aboutScreen.developer"
-          url="https://simpledex.cavender.io/" />
+          titleTx="aboutScreen.project"
+          url="https://simpledex.cavender.io" />
       </View>
-      <Image source={pokeapiLogo} style={IMAGE} />
+      <View>
+        <Text style={API_TEXT} tx="aboutScreen.poweredBy" />
+        <Image source={pokeapiLogo} style={[LOGO, API_LOGO]} />
+      </View>
     </Screen>
   )
 })
