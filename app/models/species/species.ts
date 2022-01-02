7abafc2a6c2e6b0d8ef1b5355ea4635b6b990fe7
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2022-01-02 13:03:53 -0500

    rm comments with unused code

--- app/models/species/species.ts
+++ app/models/species/species.ts
@@ -1,6 +1,5 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
 import { VarietyModel } from "../pokemon/variety"
-import { withEnvironment } from "../extensions/with-environment"
 
 /**
  * A species of Pokemon
@@ -13,21 +12,6 @@ export const SpeciesModel = types
     evolution_chain: types.maybe(types.number),
     varieties: types.maybe(types.array(VarietyModel)),
   })
-  .extend(withEnvironment)
-  .views((self) => ({
-    evolvesTo: async () => {
-      // TODO: return array of species this can evolve to
-
-      // const speciesApi = new SpeciesApi(self.environment.api)
-      // const result = await speciesApi.getAll()
-
-      // if (result.kind === "ok") {
-      //   self.save(result.species)
-      // } else {
-      //   __DEV__ && console.tron.log(result.kind)
-      // }
-    },
-  }))
 
 type SpeciesType = Instance<typeof SpeciesModel>
 export interface Species extends SpeciesType {}
