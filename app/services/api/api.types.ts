ac8ed62466616e70bc59e4a71659bb89d07d0f78
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2022-01-13 13:16:58 -0500

    stub encounter api

--- app/services/api/api.types.ts
+++ app/services/api/api.types.ts
@@ -2,6 +2,7 @@ import { GeneralApiProblem } from "./api-problem"
 import { Species } from "../../models/species/species"
 import { Pokemon } from "../../models/pokemon/pokemon"
 import { EvolutionLink } from "../../models/evolution/evolution-link"
+import { Encounter } from "../../models/encounter/encounter"
 
 // Species
 export type GetAllSpeciesResult = { kind: "ok"; species: Species[] } | GeneralApiProblem
@@ -12,3 +13,6 @@ export type GetPokemonResult = { kind: "ok"; pokemon: Pokemon } | GeneralApiProb
 
 // Evolution
 export type GetEvolutionChainResult = { kind: "ok"; chain: EvolutionLink } | GeneralApiProblem
+
+// Encounter
+export type GetEncounterResult = { kind: "ok"; encounters: Encounter[] } | GeneralApiProblem
---
+++ app/services/api/encounter-api.ts
@@ -0,0 +1,60 @@
+import { ApiResponse } from "apisauce"
+import { Api } from "./api"
+import { getGeneralApiProblem } from "./api-problem"
+import { GetEncounterResult } from "./api-types"
+
+const recurseEvolutions = (name: string, chain: GetEvolutionChainResult): any => {
+  if (name.toLowerCase() !== chain.species.name.toLowerCase()) {
+    return recurseEvolutions(name, chain.evolves_to[0])
+  }
+  chain.evolves_to.forEach((evolution) => {
+    evolution.evolution_details.forEach((details) => {
+      details.trigger = details.trigger.name
+      if (details.item) details.item = details.item.name
+      if (details.held_item) details.held_item = details.held_item.name
+      if (details.known_move) details.known_move = details.known_move.name
+      if (details.known_move_type) details.known_move_type = details.known_move_type.name
+      if (details.location) details.location = details.location.name
+      if (details.party_species) details.party_species = details.party_species.name
+      if (details.party_type) details.party_type = details.party_type.name
+      if (details.trade_species) details.trade_species = details.trade_species.name
+    })
+    evolution.evolves_to = []
+    evolution.species = evolution.species.name
+  })
+  return chain.evolves_to
+}
+
+export class EncounterApi {
+  private api: Api
+
+  constructor(api: Api) {
+    this.api = api
+  }
+
+  async getChain(pokemon: string | number): Promise<GetEncounterResult> {
+    try {
+      const response: ApiResponse<any> = await this.api.apisauce.get(`pokemon/${pokemon}/encounters`)
+
+      const convert = (raw) => {
+        return {
+          id: raw.id,
+          name: raw.name,
+        }
+      }
+
+      // the typical ways to die when calling an api
+      if (!response.ok) {
+        const problem = getGeneralApiProblem(response)
+        if (problem) return problem
+      }
+
+      const chain = recurseEvolutions(species, response.data.chain)
+
+      return { kind: "ok", chain }
+    } catch (e) {
+      __DEV__ && console.tron.log(e.message)
+      return { kind: "bad-data" }
+    }
+  }
+}
