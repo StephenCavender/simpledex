e8d26658c9cf423f1b97c1c138694d4c4a5a02fa
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2022-01-16 15:12:34 -0500

    rm observer from sprite component

--- app/components/sprite/sprite.tsx
+++ app/components/sprite/sprite.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { ImageStyle } from "react-native"
-import { observer } from "mobx-react-lite"
 import { AutoImage as Image } from "../auto-image/auto-image"
 
 const SPRITE: ImageStyle = {
@@ -13,10 +12,10 @@ export interface SpriteProps {
   uri: string
 }
 
-export const Sprite = observer(function Sprite(props: SpriteProps) {
+export const Sprite = function Sprite(props: SpriteProps) {
   const { uri } = props
 
   return (
     <Image style={SPRITE} source={{ uri }} />
   )
-})
+}
