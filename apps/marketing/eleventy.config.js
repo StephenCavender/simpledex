c350206865bff0232f7071eeeb763879f5fc6e35
Steve <github@cavender.io>
2026-05-03 16:19:21 -0400

    chore(marketing): update Eleventy config and styles

--- apps/marketing/eleventy.config.js
+++ apps/marketing/eleventy.config.js
@@ -1,14 +1,14 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/style.css');
-  eleventyConfig.addPassthroughCopy('src/images');
-  eleventyConfig.addPassthroughCopy('src/llms.txt');
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/llms.txt");
 
   return {
     dir: {
-      input: 'src',
-      includes: '_includes',
-      data: '_data',
-      output: '_site',
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
     },
   };
 };
